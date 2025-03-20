e1e56a73edf87dee802e048133711f556003f86c
Tu Ngoc Van <tungocvan@gmail.com>
2025-03-20 16:53:11 +0700

    19032025-2

--- resources/views/livewire/components/form.blade.php
+++ resources/views/livewire/components/form.blade.php
@@ -1,6 +1,6 @@
 <div>
 
-<x-adminlte-modal id="modalMin" title="Minimal">
+{{-- <x-adminlte-modal id="modalMin" title="Minimal">
     <h2>ABC</h2>
     <x-slot name="footerSlot">
         <x-adminlte-button class="mr-auto" theme="success" label="Accept"/>
@@ -8,8 +8,8 @@
     </x-slot>
 </x-adminlte-modal>    
 <x-adminlte-button label="Open Modal" data-toggle="modal" data-target="#modalMin"/>
-<hr>
-@php
+<hr> --}}
+{{-- @php
     $config = [
         "placeholder" => "Select multiple options...",
         "allowClear" => true,
@@ -29,15 +29,15 @@
     </x-adminlte-select2>
 
     <span x-text="category"></span>
-</div>
+</div> --}}
 
 
 
- <div x-data="{ categories: [] }" x-init="
- $('#sel2Category').on('change', function() {
-     categories = Array.from(this.selectedOptions).map(option => option.value);
- });
-">
+ {{-- <div x-data="{ categories: [] }" x-init="
+    $('#sel2Category').on('change', function() {
+        categories = Array.from(this.selectedOptions).map(option => option.value);
+    });
+    ">
  <x-adminlte-select2 id="sel2Category" name="sel2Category[]" label="Categories"
      label-class="text-danger" igroup-size="sm" :config="$config" multiple>
      
@@ -72,14 +72,14 @@
             data = contents; // Cập nhật nội dung vào biến Alpine
         });
     });
-">
+    ">
     <x-adminlte-text-editor name="teBasic" id="teBasic"></x-adminlte-text-editor>
 
     <p class="mt-2">📌 <strong>Nội dung đã nhập:</strong></p>
     <div class="border p-2 mt-2 bg-light" x-html="data"></div>
-</div>
+</div> --}}
 
-<div class="row">
+{{-- <div class="row">
     <div class="col-4">
         @php
         $config = ['format' => 'DD/MM/YYYY HH:mm'];
@@ -97,8 +97,8 @@
             Launch Default Toast
           </button>
     </div>  
-</div>
-<div>
+</div> --}}
+{{-- <div>
     <button x-on:click="$wire.showModal = !$wire.showModal">New Post</button> 
     <div wire:show="showModal">
         <form wire:submit="save">
@@ -137,9 +137,9 @@
         </form>
     </div>
 
-</div>
+</div> --}}
 <hr>
-<div x-data="{ openTab: 'home' }" class="row">
+{{-- <div x-data="{ openTab: 'home' }" class="row">
     <div class="col-6">
         <div x-data="{ openTab: 'home' }" class="row">
             <div class="col-5 col-sm-3">
@@ -227,9 +227,9 @@
         </div>
         <!-- /.card -->
     </div>
-</div>
+</div> --}}
 
-<div x-data="{ currentStep: 1 }" class="row">
+{{-- <div x-data="{ currentStep: 1 }" class="row">
     <div class="col-md-6">
         <div class="card p-2">
             <div class="card-header">
@@ -286,145 +286,14 @@
         </div>
         <!-- /.card -->
     </div>
-</div>
+</div> --}}
 
-<div class="row">
-    <div class="col-md-12">
-      <div class="card card-default">
-        <div class="card-header">
-          <h3 class="card-title">Dropzone.js <small><em>jQuery File Upload</em> like look</small></h3>
-        </div>
-        <div class="card-body">
-          <div id="actions" class="row">
-            <div class="col-lg-6">
-              <div class="btn-group w-100">
-                <span class="btn btn-success col fileinput-button">
-                  <i class="fas fa-plus"></i>
-                  <span>Add files</span>
-                </span>
-                <button type="submit" class="btn btn-primary col start">
-                  <i class="fas fa-upload"></i>
-                  <span>Start upload</span>
-                </button>
-                <button type="reset" class="btn btn-warning col cancel">
-                  <i class="fas fa-times-circle"></i>
-                  <span>Cancel upload</span>
-                </button>
-              </div>
-            </div>
-            <div class="col-lg-6 d-flex align-items-center">
-              <div class="fileupload-process w-100">
-                <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                  <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="table table-striped files" id="previews">
-            <div id="template" class="row mt-2">
-              <div class="col-auto">
-                  <span class="preview"><img src="{{ asset('data:,') }}" alt="" data-dz-thumbnail /></span>
-              </div>
-              <div class="col d-flex align-items-center">
-                  <p class="mb-0">
-                    <span class="lead" data-dz-name></span>
-                    (<span data-dz-size></span>)
-                  </p>
-                  <strong class="error text-danger" data-dz-errormessage></strong>
-              </div>
-              <div class="col-4 d-flex align-items-center">
-                  <div class="progress progress-striped active w-100" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
-                    <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
-                  </div>
-              </div>
-              <div class="col-auto d-flex align-items-center">
-                <div class="btn-group">
-                  <button class="btn btn-primary start">
-                    <i class="fas fa-upload"></i>
-                    <span>Start</span>
-                  </button>
-                  <button data-dz-remove class="btn btn-warning cancel">
-                    <i class="fas fa-times-circle"></i>
-                    <span>Cancel</span>
-                  </button>
-                  <button data-dz-remove class="btn btn-danger delete">
-                    <i class="fas fa-trash"></i>
-                    <span>Delete</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- /.card-body -->
-        <div class="card-footer">
-          Visit <a href="{{ asset('https://www.dropzonejs.com') }}">dropzone.js documentation</a> for more examples and information about the plugin.
-        </div>
-      </div>
-      <!-- /.card -->
-    </div>
-  </div>
+
+
+@livewire('uploads')
+
   <!-- /.row -->
 </div>
 
-@script
-<script>
-    document.addEventListener('livewire:initialized', () => {
-    // DropzoneJS Demo Code Start
-  Dropzone.autoDiscover = false
-
-// Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
-var previewNode = document.querySelector("#template")
-previewNode.id = ""
-var previewTemplate = previewNode.parentNode.innerHTML
-previewNode.parentNode.removeChild(previewNode)
-
-var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
-  url: "/upload", // Set the url
-  thumbnailWidth: 80,
-  thumbnailHeight: 80,
-  parallelUploads: 20,
-  previewTemplate: previewTemplate,
-  autoQueue: false, // Make sure the files aren't queued until manually added
-  previewsContainer: "#previews", // Define the container to display the previews
-  clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
-})
-
-myDropzone.on("addedfile", function(file) {
-  // Hookup the start button
-  file.previewElement.querySelector(".start").onclick = function() {console.log('start'); myDropzone.enqueueFile(file) }
-})
-
-// Update the total progress bar
-myDropzone.on("totaluploadprogress", function(progress) {
-  document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
-})
-
-myDropzone.on("sending", function(file) {
-  // Show the total progress bar when upload starts
-  document.querySelector("#total-progress").style.opacity = "1"
-  // And disable the start button
-  file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
-})
-
-// Hide the total progress bar when nothing's uploading anymore
-myDropzone.on("queuecomplete", function(progress) {
-  document.querySelector("#total-progress").style.opacity = "0"
-})
-
-// Setup the buttons for all transfers
-// The "add files" button doesn't need to be setup because the config
-// `clickable` has already been specified.
-document.querySelector("#actions .start").onclick = function() {
-   console.log('start upload');
-    //myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
-}
-document.querySelector("#actions .cancel").onclick = function() {
-  myDropzone.removeAllFiles(true)
-}
-// DropzoneJS Demo Code End
-})
-</script>
-@endscript
 
 </div>
---
+++ resources/views/livewire/uploads.blade.php
@@ -0,0 +1,187 @@
+<?php
+
+use Livewire\Volt\Component;
+use Livewire\WithFileUploads;
+use Livewire\Attributes\Validate;
+
+new class extends Component {
+    use WithFileUploads;
+     #[Validate('image|max:1024')] // 1MB Max
+     public $photo;
+     public $successMessage;
+
+     public function save($file)
+    {
+        
+        //dd($file[0]);
+        // Kiểm tra xem có dữ liệu trong file không
+        if (isset($file[0]['dataURL'])) {
+            // Tạo một tên tệp và lưu trữ tệp từ base64
+            $data = $file[0]['dataURL'];
+            $filename = $file[0]['upload']['filename'];
+
+            // Tách base64 và loại bỏ phần không cần thiết
+            list($type, $data) = explode(';', $data);
+            list(, $data)      = explode(',', $data);
+            $data = base64_decode($data);
+
+            // Lưu tệp vào thư mục 'photos'
+            file_put_contents(public_path('photos/' . $filename), $data);
+            
+            // Thiết lập thông báo thành công
+            $this->successMessage = 'Ảnh đã được tải lên thành công!';
+        } else {
+            // Xử lý lỗi nếu không có dữ liệu
+            $this->addError('photo', 'Không có tệp nào được chọn!');
+        }
+    }
+}; ?>
+
+<div class="row">
+    <div class="col-md-12">
+      <div class="card card-default">
+        <div class="card-header">
+          <h3 class="card-title">Dropzone.js <small><em>jQuery File Upload</em> like look</small></h3>
+        </div>
+        <div class="card-body">
+          <div id="actions" class="row">
+            <div class="col-lg-6">
+              <div class="btn-group w-100">
+                <span class="btn btn-success col fileinput-button" x-on:click="open = ! open" >
+                  <i class="fas fa-plus"></i>
+                  <span>Add files</span>
+                </span>
+                <button type="submit" class="btn btn-primary col start">
+                  <i class="fas fa-upload"></i>
+                  <span>Start upload</span>
+                </button>
+                <button type="reset" class="btn btn-warning col cancel">
+                  <i class="fas fa-times-circle"></i>
+                  <span>Cancel upload</span>
+                </button>
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center">
+              <div class="fileupload-process w-100">
+                <div id="total-progress" class="progress progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
+                  <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="table table-striped files" id="previews">
+            <div id="template" class="row mt-2">
+              
+              <div class="col-auto">
+                  <span class="preview">                                                       
+                        <img src="{{ asset('data:,') }}" alt="" data-dz-thumbnail />                                                              
+                 </span>
+              </div>
+              <div class="col d-flex align-items-center">
+                  <p class="mb-0">
+                    <span class="lead" data-dz-name></span>
+                    (<span data-dz-size></span>)
+                  </p>
+                  <strong class="error text-danger" data-dz-errormessage></strong>
+              </div>
+              <div class="col-4 d-flex align-items-center">
+                  <div class="progress progress-striped active w-100" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0">
+                    <div class="progress-bar progress-bar-success" style="width:0%;" data-dz-uploadprogress></div>
+                  </div>
+              </div>
+              <div class="col-auto d-flex align-items-center">
+                <div class="btn-group">
+                  <button class="btn btn-primary start">
+                    <i class="fas fa-upload"></i>
+                    <span>Start</span>
+                  </button>
+                  <button data-dz-remove class="btn btn-warning cancel">
+                    <i class="fas fa-times-circle"></i>
+                    <span>Cancel</span>
+                  </button>
+                  <button data-dz-remove class="btn btn-danger delete">
+                    <i class="fas fa-trash"></i>
+                    <span>Delete</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /.card-body -->
+        <div class="card-footer">
+          Visit <a href="{{ asset('https://www.dropzonejs.com') }}">dropzone.js documentation</a> for more examples and information about the plugin.
+        </div>
+      </div>
+      <!-- /.card -->
+    </div>
+    
+    @script
+    <script>
+        var show  = false   
+        document.addEventListener('livewire:initialized', () => {
+        // DropzoneJS Demo Code Start
+      Dropzone.autoDiscover = false
+      
+    // Get the template HTML and remove it from the doumenthe template HTML and remove it from the doument
+    var previewNode = document.querySelector("#template")
+    previewNode.id = ""
+    var previewTemplate = previewNode.parentNode.innerHTML
+    previewNode.parentNode.removeChild(previewNode)
+    
+    var myDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+      url: "#", // Set the url
+      thumbnailWidth: 80,
+      thumbnailHeight: 80,
+      parallelUploads: 20,
+      previewTemplate: previewTemplate,
+      autoQueue: false, // Make sure the files aren't queued until manually added
+      previewsContainer: "#previews", // Define the container to display the previews
+      clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+    })
+    
+    
+
+    myDropzone.on("addedfile", function(file) {
+      // Hookup the start button
+      file.previewElement.querySelector(".start").onclick = function() {
+        console.log('start'); 
+        $wire.open = true;
+        $wire.save([file])    
+     // myDropzone.enqueueFile(file) 
+    }
+    })
+    
+    // Update the total progress bar
+    myDropzone.on("totaluploadprogress", function(progress) {
+      document.querySelector("#total-progress .progress-bar").style.width = progress + "%"
+    })
+    
+    myDropzone.on("sending", function(file) {
+      // Show the total progress bar when upload starts
+      document.querySelector("#total-progress").style.opacity = "1"
+      // And disable the start button
+      file.previewElement.querySelector(".start").setAttribute("disabled", "disabled")
+    })
+    
+    // Hide the total progress bar when nothing's uploading anymore
+    myDropzone.on("queuecomplete", function(progress) {
+      document.querySelector("#total-progress").style.opacity = "0"
+    })
+    
+    // Setup the buttons for all transfers
+    // The "add files" button doesn't need to be setup because the config
+    // `clickable` has already been specified.
+    document.querySelector("#actions .start").onclick = function() {
+       console.log('start upload');
+        //myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED))
+    }
+    document.querySelector("#actions .cancel").onclick = function() {
+      myDropzone.removeAllFiles(true)
+    }
+    // DropzoneJS Demo Code End
+    })
+    </script>
+    @endscript
+
+</div>
